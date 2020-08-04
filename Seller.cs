96eb8aed62cae76338ce59286bd67c9c8674132e
jeniashasha <66595992+jeniashasha@users.noreply.github.com>
2020-08-04 18:37:22 +0300

    Add files via upload

---
+++ Seller.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NewsCompany;
+namespace Store
+{
+    class Seller
+    {
+        private int moneyEarned = 100;
+
+        Newspaper newspaper = new Newspaper();
+         float newPrice = 50f;
+
+        public void SellNewspaper(Newspaper newspaper)
+        {
+
+            this.moneyEarned = (int)(moneyEarned + newspaper.GetPrice());    
+        }
+
+        public override string ToString()
+        {
+            return $"{ base.ToString()} money earned : {moneyEarned}";
+
+
+        }
+    }
+}
